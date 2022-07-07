dea50b319ee199c4a509afc65d8ab4551b366105
Jan Pierre <gamunaxx@gmail.com>
2022-07-07 14:43:01 -0500

    style: 💄 add style to header

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
-
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
-export class HomeModule { }
+export class HomeModule {}
